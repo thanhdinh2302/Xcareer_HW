6d23c0f5152419fc32c26c76b2b77a151b6fd5a4
thanhdinh2302 <61083772+thanhdinh2302@users.noreply.github.com>
2021-01-05 17:04:13 +0700

    Delete largestNumber.js

--- jan5th/largestNumber.js
+++
@@ -1,8 +0,0 @@
-function largestNumber(n) {
-  if (n < 1 || n > 9) {
-    alert("Your input can only be in the range of 1 to 9 ");
-  } else {
-    return Math.pow(10, n) - 1;
-  }
-}
-console.log(largestNumber(6));
